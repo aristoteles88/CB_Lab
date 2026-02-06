a166b9d53f28a09ad62f75da92086d7fbf67e3e6
aristoteles88 <aristoteles88@gmail.com>
2026-02-06 09:27:56 -0300

    Cria funções de autenticação

---
+++ backend/app/auth.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+import os
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    try:
+        to_encode = data.copy()
+        expire = datetime.now() + (expires_delta or timedelta(minutes=30))
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    except JWTError:
+        raise ValueError("Falha ao criar token JWT")
+    return encoded_jwt
