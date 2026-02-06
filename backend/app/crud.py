912ab2ea857ebdb799742a59683a6aaf7ee8c280
aristoteles88 <aristoteles88@gmail.com>
2026-02-06 09:42:55 -0300

    Cria crud.py com as funcões de criação e leitura de um usuário.

---
+++ backend/app/crud.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+from . import models, schemas, auth
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = auth.hash_password(user.password)
+    db_user = models.User(
+        name=user.name,
+        email=user.email,
+        hashed_password=hashed_password,
+        is_superuser=user.is_superuser
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def list_users(db: Session):
+    return db.query(models.User).all()
